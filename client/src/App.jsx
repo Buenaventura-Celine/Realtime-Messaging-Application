39af4009c92971d54a5704ece21e617fc29ed6aa
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-09-18 11:00:50 +0800

    Form for login/signup - client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { StreamChat } from 'stream-chat'
 import { Chat } from 'stream-chat-react'
 import Cookies from 'universal-cookie'
-import { ChannelContainer, ChannelListContainer } from './components'
+import { ChannelContainer, ChannelListContainer, Auth } from './components'
 import './App.css'
 
 const apiKey = 'w3b5gn6gvccu'
 
 const client = StreamChat.getInstance(apiKey)
 
+const authToken = false;
+
 const App = () => {
+    if(!authToken) return <Auth/>
     return (
         <div className="app__wrapper">
             <Chat client={client} theme="team light">
