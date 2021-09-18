a9190dc563ed4f14929ae73780b1658131c03cd9
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-09-18 08:52:42 +0800

    Generating components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { StreamChat } from 'stream-chat'
+import { Chat } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+import { ChannelContainer, ChannelListContainer } from './components'
+
+const apiKey = 'w3b5gn6gvccu'
+
+const client = StreamChat.getInstance(apiKey)
 
 const App = () => {
     return (
-        <div>
-            <h1>Logist</h1>
+        <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer/>
+                <ChannelContainer/>
+            </Chat>
         </div>
     )
 }
