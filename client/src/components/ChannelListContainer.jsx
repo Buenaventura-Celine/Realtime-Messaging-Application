e15164c1046e3de99938d384726de9f879047396
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-09-18 09:16:09 +0800

    Styles for channel list container added

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -4,6 +4,7 @@ import { ChannelList, useChatContext } from 'stream-chat-react'
 import Cookies from 'universal-cookie'
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './'
 import HospitalIcon from '../assets/hospital.png'
+import LogoutIcon from '../assets/logout.png'
 
 const SideBar = () => (
     <div className="channel-list__sidebar">
@@ -12,14 +13,28 @@ const SideBar = () => (
                 <img src={HospitalIcon} alt="Hospital" width="30"/>
             </div>
         </div>
+        <div className="channel-list__sidebar__icon2">
+            <div className="icon1__inner">
+                <img src={LogoutIcon} alt="Logout" width="30"/>
+            </div>
+        </div>
     </div>
-)
+);
+
+const CompanyHeader = () => (
+    <div className="channel-list__header">
+        <p className="channel-list__header__text">Logist</p>
+    </div>
+);
 
 const ChannelListContainer = () => {
     return (
-        <div>
-            Channel List Container
-        </div>
+        <>
+            <SideBar/>
+            <div className="channel-list__list__wrapper">
+                <CompanyHeader/>
+            </div>
+        </>
     )
 }
 
